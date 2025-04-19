8c1511d6f84a8b4fe8213e69629de4dd90be49dd
Alex Swift <alex.jones@jellybaby.com>
2025-04-19 03:09:09 +0100

    update setup to write control (hopefully)

--- reactor-setup.lua
+++ reactor-setup.lua
@@ -1,4 +1,4 @@
--- command: setup <relay | control> <reactorQuadrant> <reactorTriplet> <<reactorNumber>
+-- command: setup <relay | control> <reactorQuadrant> <reactorTriplet> <reactorNumber>
 local tArgs = { ... }
 
 local type = tArgs[1]
@@ -7,14 +7,39 @@ local reactorTriplet = tArgs[3]
 local reactorNumber = tArgs[4]
 
 -- Download wpp
-shell.run("wget https://raw.githubusercontent.com/jdf221/CC-WirelessPeripheral/main/wpp.lua wpp"); 
+shell.run("wget https://raw.githubusercontent.com/jdf221/CC-WirelessPeripheral/main/wpp.lua wpp")
 
 if type == "relay" then
     shell.run("wget https://raw.githubusercontent.com/ajjswift/mccc/refs/heads/main/reactor-relay.lua startup.lua")
-end
-if type == "control" then
-    shell.run("wget https://raw.githubusercontent.com/ajjswift/mccc/refs/heads/main/reactor-control.lua startup.lua");
-end
+elseif type == "control" then
+    -- Download the reactor-control.lua file
+    shell.run("wget https://raw.githubusercontent.com/ajjswift/mccc/refs/heads/main/reactor-control.lua temp_reactor_control.lua")
 
+    -- Read the content of the downloaded file
+    local file = io.open("temp_reactor_control.lua", "r")
+    if not file then
+        print("Error: Could not open temp_reactor_control.lua for reading")
+        return
+    end
+    local content = file:read("a")
+    file:close()
+
+    -- Modify the content to set the variables
+    content = string.gsub(content, "local reactorNumber = .*", "local reactorNumber = " .. reactorNumber)
+    content = string.gsub(content, "local reactorQuadrant = \".*\"", "local reactorQuadrant = \"" .. reactorQuadrant .. "\"")
+    content = string.gsub(content, "local reactorTriplet = \".*\"", "local reactorTriplet = \"" .. reactorTriplet .. "\"")
+
+    -- Write the modified content to startup.lua
+    file = io.open("startup.lua", "w")
+    if not file then
+        print("Error: Could not open startup.lua for writing")
+        return
+    end
+    file:write(content)
+    file:close()
+
+    -- Delete the temporary file
+    fs.delete("temp_reactor_control.lua")
+end
 
 -- shell.execute("reboot");
