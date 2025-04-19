ec5a79c7c96de2fa1514849644b31d30847a03b1
Alex Swift <alex.jones@jellybaby.com>
2025-04-19 02:23:33 +0100

    Initial push

---
+++ reactor-control.lua
@@ -0,0 +1,34 @@
+local reactorNumber = 1
+local reactorQuadrant = "lc"
+local reactorTriplet = "b"
+
+local wpp = require("wpp");
+wpp.wireless.connect(string.format("reactor-management-%s-%s-%d", reactorQuadrant, reactorTriplet, reactorNumber))
+
+local reactor = wpp.peripheral.find("fusionReactorLogidAdapter");
+
+local monitor = peripheral.wrap("right");
+
+local w,h = monitor.getSize()
+
+
+
+while true do
+    local reactorName = string.format("Reactor rc-%s-%s-%d", reactorQuadrant, reactorTriplet, reactorNumber);
+    local productionRate = reactor.getProductionRate() * 0.4
+    local injectionRate = reactor.getInjectionRate()
+
+    monitor.clear()
+    monitor.setCursorPos(3,2)
+    monitor.write(reactorName)
+    monitor.setCursorPos(3, 3)
+    monitor.write(
+        string.format("Output: %d FE/t", productionRate)
+    )
+
+    monitor.setCursorPos(3,4)
+    monitor.write(
+        string.format("Injection rate: %d mB/t", injectionRate)
+    )
+    os.sleep(0.5)
+end
