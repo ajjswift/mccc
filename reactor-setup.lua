b2d2be04137f56aa2ad2560997ae1b4f7cecedd7
Alex Swift <alex.jones@jellybaby.com>
2025-04-19 03:00:36 +0100

    Test setup

---
+++ reactor-setup.lua
@@ -0,0 +1,20 @@
+-- command: setup <relay | control> <reactorQuadrant> <reactorTriplet> <<reactorNumber>
+local tArgs = { ... }
+
+local type = tArgs[1]
+local reactorQuadrant = tArgs[2]
+local reactorTriplet = tArgs[3]
+local reactorNumber = tArgs[4]
+
+-- Download wpp
+shell.run("wget https://raw.githubusercontent.com/jdf221/CC-WirelessPeripheral/main/wpp.lua wpp"); 
+
+if type == "relay" then
+    shell.run("wget https://raw.githubusercontent.com/ajjswift/mccc/refs/heads/main/reactor-relay.lua startup.lua")
+end
+if type == "control" then
+    shell.run("wget https://raw.githubusercontent.com/ajjswift/mccc/refs/heads/main/reactor-control.lua startup.lua");
+end
+
+
+shell.execute("reboot");
