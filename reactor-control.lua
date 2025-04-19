99270f7c49e9be3a8d7530e6eff9d7b4dcaa1576
Alex Swift <alex.jones@jellybaby.com>
2025-04-19 02:29:30 +0100

    Fix spelling mistake

--- reactor-control.lua
+++ reactor-control.lua
@@ -12,23 +12,24 @@ local monitor = peripheral.wrap("right");
 local w,h = monitor.getSize()
 
 
+if reactor then
+    while true do
+        local reactorName = string.format("Reactor rc-%s-%s-%d", reactorQuadrant, reactorTriplet, reactorNumber);
+        local productionRate = reactor.getProductionRate() * 0.4
+        local injectionRate = reactor.getInjectionRate()
 
-while true do
-    local reactorName = string.format("Reactor rc-%s-%s-%d", reactorQuadrant, reactorTriplet, reactorNumber);
-    local productionRate = reactor.getProductionRate() * 0.4
-    local injectionRate = reactor.getInjectionRate()
+        monitor.clear()
+        monitor.setCursorPos(3,2)
+        monitor.write(reactorName)
+        monitor.setCursorPos(3, 3)
+        monitor.write(
+            string.format("Output: %d FE/t", productionRate)
+        )
 
-    monitor.clear()
-    monitor.setCursorPos(3,2)
-    monitor.write(reactorName)
-    monitor.setCursorPos(3, 3)
-    monitor.write(
-        string.format("Output: %d FE/t", productionRate)
-    )
-
-    monitor.setCursorPos(3,4)
-    monitor.write(
-        string.format("Injection rate: %d mB/t", injectionRate)
-    )
-    os.sleep(0.5)
+        monitor.setCursorPos(3,4)
+        monitor.write(
+            string.format("Injection rate: %d mB/t", injectionRate)
+        )
+        os.sleep(0.5)
+    end
 end
